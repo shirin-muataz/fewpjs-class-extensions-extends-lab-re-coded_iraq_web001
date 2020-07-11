d757f584ca9a85ad97043ffbdfaf17a7f4977cf8
Shirin Muataz <shirinmuataz@gmail.com>
2020-07-11 08:26:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+
+class Polygon {
+    constructor (intArr){
+        this.intArr=intArr;
+    } 
+    get countSides()
+    { return this.intArr.length;}
+    
+    get perimeter()
+    {
+      return (this.intArr.reduce((x,y)=>{ return x+y;}))
+    }
+  
+}
+
+class Triangle extends Polygon {
+get isValid (){
+  
+}
+}
